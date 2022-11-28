fb25f5aef88d071128e117cd1d296dfb2912abb6
fluttermiddlepodcast <fluttermiddlepodcast@gmail.com>
2022-11-28 20:02:37 +0300

    added "Meeting"

---
+++ lib/6kyu/meeting.dart
@@ -0,0 +1,9 @@
+String meeting(String s) {
+  final arr = s.toUpperCase()
+      .split(';')
+      .map((s) => s.split(':'))
+      .map((s) => '(${s[1]}, ${s[0]})')
+      .toList();
+  arr.sort();
+  return arr.join();
+}
