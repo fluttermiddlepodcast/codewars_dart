e28050e845242faf23d3ac5d34d063106320b6ba
fluttermiddlepodcast <fluttermiddlepodcast@gmail.com>
2022-12-03 10:52:12 +0300

    added "Mumbling"

---
+++ test/7kyu/mumbling_test.dart
@@ -0,0 +1,92 @@
+import 'package:test/test.dart';
+import 'package:codewars_dart/7kyu/mumbling.dart';
+
+void main() {
+  void testing(String str, String exp) => test(
+        'Testing for $str',
+        () => expect(accum(str), equals(exp)),
+      );
+
+  group('Fixed tests', () {
+    testing(
+      'ZpglnRxqenU',
+      'Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu',
+    );
+    testing(
+      'NyffsGeyylB',
+      'N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb',
+    );
+    testing(
+      'MjtkuBovqrU',
+      'M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu',
+    );
+    testing(
+      'EvidjUnokmM',
+      'E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm',
+    );
+    testing(
+      'HbideVbxncC',
+      'H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc',
+    );
+    testing(
+      'VwhvtHtrxfE',
+      'V-Ww-Hhh-Vvvv-Ttttt-Hhhhhh-Ttttttt-Rrrrrrrr-Xxxxxxxxx-Ffffffffff-Eeeeeeeeeee',
+    );
+    testing(
+      'KurgiKmkphY',
+      'K-Uu-Rrr-Gggg-Iiiii-Kkkkkk-Mmmmmmm-Kkkkkkkk-Ppppppppp-Hhhhhhhhhh-Yyyyyyyyyyy',
+    );
+    testing(
+      'NctlfBlnmfH',
+      'N-Cc-Ttt-Llll-Fffff-Bbbbbb-Lllllll-Nnnnnnnn-Mmmmmmmmm-Ffffffffff-Hhhhhhhhhhh',
+    );
+    testing(
+      'WegunHvbdmV',
+      'W-Ee-Ggg-Uuuu-Nnnnn-Hhhhhh-Vvvvvvv-Bbbbbbbb-Ddddddddd-Mmmmmmmmmm-Vvvvvvvvvvv',
+    );
+    testing(
+      'VoywwSpqidE',
+      'V-Oo-Yyy-Wwww-Wwwww-Ssssss-Ppppppp-Qqqqqqqq-Iiiiiiiii-Dddddddddd-Eeeeeeeeeee',
+    );
+    testing(
+      'VbaixFpxdcO',
+      'V-Bb-Aaa-Iiii-Xxxxx-Ffffff-Ppppppp-Xxxxxxxx-Ddddddddd-Cccccccccc-Ooooooooooo',
+    );
+    testing(
+      'OlyqvYwkuzF',
+      'O-Ll-Yyy-Qqqq-Vvvvv-Yyyyyy-Wwwwwww-Kkkkkkkk-Uuuuuuuuu-Zzzzzzzzzz-Fffffffffff',
+    );
+    testing(
+      'JrhfdMtchiH',
+      'J-Rr-Hhh-Ffff-Ddddd-Mmmmmm-Ttttttt-Cccccccc-Hhhhhhhhh-Iiiiiiiiii-Hhhhhhhhhhh',
+    );
+    testing(
+      'JiwpcSwslvW',
+      'J-Ii-Www-Pppp-Ccccc-Ssssss-Wwwwwww-Ssssssss-Lllllllll-Vvvvvvvvvv-Wwwwwwwwwww',
+    );
+    testing(
+      'EagpiEvmabJ',
+      'E-Aa-Ggg-Pppp-Iiiii-Eeeeee-Vvvvvvv-Mmmmmmmm-Aaaaaaaaa-Bbbbbbbbbb-Jjjjjjjjjjj',
+    );
+    testing(
+      'RznlcEmuxxP',
+      'R-Zz-Nnn-Llll-Ccccc-Eeeeee-Mmmmmmm-Uuuuuuuu-Xxxxxxxxx-Xxxxxxxxxx-Ppppppppppp',
+    );
+    testing(
+      'OrggaExarzP',
+      'O-Rr-Ggg-Gggg-Aaaaa-Eeeeee-Xxxxxxx-Aaaaaaaa-Rrrrrrrrr-Zzzzzzzzzz-Ppppppppppp',
+    );
+    testing(
+      'DriraMtedfB',
+      'D-Rr-Iii-Rrrr-Aaaaa-Mmmmmm-Ttttttt-Eeeeeeee-Ddddddddd-Ffffffffff-Bbbbbbbbbbb',
+    );
+    testing(
+      'BjxseRxgtjT',
+      'B-Jj-Xxx-Ssss-Eeeee-Rrrrrr-Xxxxxxx-Gggggggg-Ttttttttt-Jjjjjjjjjj-Ttttttttttt',
+    );
+    testing(
+      'EquhxOswchE',
+      'E-Qq-Uuu-Hhhh-Xxxxx-Oooooo-Sssssss-Wwwwwwww-Ccccccccc-Hhhhhhhhhh-Eeeeeeeeeee',
+    );
+  });
+}
