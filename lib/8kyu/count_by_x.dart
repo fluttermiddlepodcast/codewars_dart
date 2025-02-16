09f5153b3af8d513811626bf9e2fb8a69fdb7a0b
fluttermiddlepodcast <fluttermiddlepodcast@gmail.com>
2025-02-16 10:27:53 +0300

    fixed Analyzer issues

--- lib/8kyu/count_by_x.dart
+++ lib/8kyu/count_by_x.dart
@@ -1,3 +1,3 @@
 List<int> countBy(int x, int n) {
-  return [for (var i = 1; i <= n; i++) i].map((num) => num * x).toList();
+  return [for (var i = 1; i <= n; i++) i].map((n) => n * x).toList();
 }
